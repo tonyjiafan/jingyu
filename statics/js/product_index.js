1c9106d90165a8c9584d1b8493575de5f69495fd
Tonyjiafan <358982833@qq.com>
2018-02-05 19:57:56 +0800

    完成 用户登录后显示的 控件和逻辑   在产品功能页面

--- statics/js/product_index.js
+++ statics/js/product_index.js
@@ -1,3 +1,28 @@
 $(function() {
-  console.log($.fn)
+  // 用户头像弹窗
+  $('#user-box').data('flag', true).on('click', function(e) {
+    // console.log(e.target)
+    if ($(e.target).attr('name') == 'userClick') {
+      if($(this).data('flag')){
+        $('#triangle-tag').css({
+          transform: 'rotateX(180deg)'
+        })
+        $('#user-settings-box').fadeIn(50)
+        $(this).data('flag', false)
+      } else {
+        $('#triangle-tag').css({
+          transform: 'rotateX(360deg)'
+        })
+        $('#user-settings-box').fadeOut(200)
+        $(this).data('flag', true)
+      }
+    } else {
+      $('#user-settings-box').css('display', 'none')
+    }
+  })
+  // 用户设置
+  $('.user-item-text').on('click', function(e) {
+    console.log($(e.target).text())
+    location.href = 'product_details.html'
+  })
 })
